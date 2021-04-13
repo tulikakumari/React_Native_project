d8e6a7225199cec6d202576738d37b44e4ecd653
Tulika Kumari <tulikakashyap1999@gmail.com>
2021-04-14 01:00:17 +0530

    Created Basic price tag and image layout

--- App.js
+++ App.js
@@ -1,91 +1,91 @@
- import React,{Component} from 'react'
-// import React from 'react'
-//  import List from './List.js'
-import {View,Text,FlatList} from 'react-native'
-import {Card} from 'react-native-paper';
+ 
+import React from 'react'
+ import List from './List.js'
+// import {View,Text} from 'react-native';
+// import {Card} from 'react-native-paper';
 
 
 
-export default class App extends Component{
-  constructor(props)
-  {
-    super(props)
+// export default class App extends Component{
+//   constructor(props)
+//   {
+//     super(props)
 
-    this.state = {
-      CryptoList:[
-        {
-           id: 0,
-          //  imgUrl: '',
-          name: 'Bitcoin',
-       },
-       {
-          id: 1,
-          // imgUrl:'',
-          name: 'Ethereum',
-       },
-       {
-          id: 2,
-          // imgUrl:'',
-          name: 'Binance Coin',
-       },
-       {
-          id: 3,
-          // imgUrl:'',
-          name: 'XRP',
-       },
-       {
-          id: 4,
-          // imgUrl:'',
-          name: 'Tether',
-       },
-       {
-          id: 5,
-          // imgUrl:'',
-          name: 'Cardano',
-       },
-       {
-          id: 6,
-          // imgUrl:'',
-          name: 'Polkadot',
-       },
-       {
-           id:7,
-          //  imgUrl:'',
-           name: 'Uniswap',
-       }
+//     this.state = {
+//       CryptoList:[
+//         {
+//            id: 0,
+//           //  imgUrl: '',
+//           name: 'Bitcoin',
+//        },
+//        {
+//           id: 1,
+//           // imgUrl:'',
+//           name: 'Ethereum',
+//        },
+//        {
+//           id: 2,
+//           // imgUrl:'',
+//           name: 'Binance Coin',
+//        },
+//        {
+//           id: 3,
+//           // imgUrl:'',
+//           name: 'XRP',
+//        },
+//        {
+//           id: 4,
+//           // imgUrl:'',
+//           name: 'Tether',
+//        },
+//        {
+//           id: 5,
+//           // imgUrl:'',
+//           name: 'Cardano',
+//        },
+//        {
+//           id: 6,
+//           // imgUrl:'',
+//           name: 'Polkadot',
+//        },
+//        {
+//            id:7,
+//           //  imgUrl:'',
+//            name: 'Uniswap',
+//        }
 
-      ]
-    }
-  }
-      render()
-      {
-        return (
-          <View style={{flex:1 , padding:5}}>
-              <FlatList
-                  data = {this.state.CryptoList}
-                  renderItem = {({item})=>{
-                      <Card>
-                        <View style={{flex:1, flexDirection:'row',padding:10}}>
-                          <Text>{item.id}</Text>
-                          <Text>{item.name}</Text>
-                        </View>
-                      </Card>
-                  }
+//       ]
+//     }
+//   }
+//       render()
+//       {
+//         return (
+//           <View style={{flex:1 , padding:5}}>
+//               <List
+//                   data = {this.state.CryptoList}
+//                   renderItem = {({item})=>{
+//                       <Card>
+//                         <View style={{flex:1, flexDirection:'row',padding:10}}>
+//                           <Text>{item.id}</Text>
+//                           <Text>{item.name}</Text>
+//                         </View>
+//                       </Card>
+//                   }
                 
-                }
-                keyExtractor = {item=>item.id}
-              />
-          </View>
-        )
-      }
-}
-
-// const App = () => {
-//    return (
-//       <List />
-//    )
+//                 }
+//                 keyExtractor = {item=>item.id}
+//               />
+//           </View>
+//         )
+//       }
 // }
-// export default App
+
+const App = () => {
+   return (
+      <List />
+   )
+}
+export default App
 
 
 
