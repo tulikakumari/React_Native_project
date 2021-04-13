af3bec95bf45cdb23a7d71c1d13b66dc8c0562ba
Tulika Kumari <tulikakashyap1999@gmail.com>
2021-04-13 21:53:28 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Button from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +20,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+// export default class App extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//     name: 'World!',
+//     }
+//   }
+//   onClick = () => {
+//     this.setState({
+//     name: 'John!',
+//     })
+//   };
+//   render() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Hello {this.state.name}</Text>
+//       <Button 
+//            onPress={() => {this.onClick()}} 
+//            title='Click me'
+//            color='#4169E1'>
+//       </Button>
+//     </View>
+//   );
+//   }
+// }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   }, 
+//   nameText: {
+//     fontSize: 50,
+//     padding: 15,
+//   }
+// });
